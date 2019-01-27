04d3ada53d02ecc36bbe5687512ff1bfcafb36b6
Aster <galaster@foxmail.com>
2019-01-27 18:36:24 +0800

    项目::First Blood!

---
+++ .vuepress/config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    title: 'VuTeX',
+    theme: '@vutex/vuepress-theme-vutex',
+    description: '',
+    dest: './static/',
+    serviceWorker: true,
+    evergreen: true,
+    themeConfig: {
+        github: 'GalAster',
+        logo: 'https://vuejs.org/images/logo.png',
+        accentColor: '#ac0b39',
+        per_page: 10,
+        lastUpdated: 'Last Updated', // string | boolean
+        date_format: 'yyyy-MM-dd HH:mm:ss',
+        nav: [
+            { text: 'Home', link: '/', root: true },
+            { text: 'Tags', link: '/tag/', tags: true },
+            { text: 'Github', link: 'https://github.com/Moe-Net/VuTeX' },
+        ],
+        comment: {
+            clientID: 'uuid',
+            clientSecret: 'uuuuuuuuuuuuuuuuuuuuuuuuuuuid',
+            repo: 'repo',
+            owner: 'owner',
+            admin: ['owner'],
+            perPage: 5,
+            // id: this.$site,      // Ensure uniqueness and length less than 50
+            distractionFreeMode: false, // Facebook-like distraction free mode
+        },
+    }
+};
